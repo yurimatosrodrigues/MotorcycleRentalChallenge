ca05a54a97ab59e6353f15b9436c9a4ebb29ec0c
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2025-10-05 13:14:58 -0300

    feat: create consumer project for receive messages

---
+++ MotorcycleRentalChallenge.Worker/Program.cs
@@ -0,0 +1,7 @@
+using MotorcycleRentalChallenge.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
